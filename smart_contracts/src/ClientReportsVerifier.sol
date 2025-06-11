7664fc7a9ed4c0b2c4e2e4be8f018b2dc20f3bd7
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-06-12 00:58:22 +0530

    Contract Import and Interfaces

---
+++ smart_contracts/src/ClientReportsVerifier.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Common} from "@chainlink/contracts/src/v0.8/llo-feeds/libraries/Common.sol";
+import {IVerifierFeeManager} from "@chainlink/contracts/src/v0.8/llo-feeds/v0.3.0/interfaces/IVerifierFeeManager.sol";
+import {IERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
+
+using SafeERC20 for IERC20;
+
+interface IVerifierProxy {
+    function verify(
+        bytes calldata payload,
+        bytes calldata parameterPayload
+    ) external payable returns (bytes memory verifierResponse);
+
+    function verifyBulk(
+        bytes[] calldata payloads,
+        bytes calldata parameterPayload
+    ) external payable returns (bytes[] memory verifierReports);
+
+    function s_feeManager() external view returns (IVerifierFeeManager);
+}
+
+interface IFeeManager {
+    function getFeeAndReward(
+        address subscriber,
+        bytes memory unverifiedReport,
+        address quoteAddress
+    ) external returns (Common.Asset memory, Common.Asset memory, uint256);
+
+    function i_linkAddress() external view returns (address);
+    function i_nativeAddress() external view returns (address);
+    function i_rewardManager() external view returns (address);
+}
+
+contract Counter {
+    
+}

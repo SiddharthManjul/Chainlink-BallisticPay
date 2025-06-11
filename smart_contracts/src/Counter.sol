6f9184085ad8d705276e3e3bd898ba3496d6abda
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-06-11 18:26:29 +0530

    Foundry Initialized Successfully

--- smart_contracts/src/Counter.sol
+++ smart_contracts/src/Counter.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Common} from "@chainlink/contracts/src/v0.8/llo-feeds/libraries/Common.sol";
+import {IVerifierFeeManager} from "@chainlink/contracts/src/v0.8/llo-feeds/v0.3.0/interfaces/IVerifierFeeManager.sol";
+import {IERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract Counter {
     uint256 public number;
 
